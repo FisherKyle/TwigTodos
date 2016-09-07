d81aa321d371c978946bae2822780bb4faba49cd
Kyle Fisher and Zachary Baum <zachary@>
2016-09-07 09:47:45 -0400

    upload initial file structure, display work with saving objects in silex.

---
+++ app/app.php
@@ -0,0 +1,33 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Task.php";
+
+    session_start();
+    if (empty($_SESSION['list_of_tasks'])) {
+        $_SESSION['list_of_tasks'] = array();
+    }
+
+    $app = new Silex\Application();
+
+    $app->get("/", function() {
+
+        $output = "";
+
+        foreach (Task::getAll() as $task) {
+            $output .= "<p>" . $task->getDescription() . "</p>";
+        }
+
+        $output .= "</ul>
+            <form action='/tasks' method='post'>
+                <label for='description'>Task Description</label>
+                <input id='description' name='description' type='text'>
+
+                <button type='submit'>Add task</button>
+            </form>
+        ";
+
+        return $output;
+    });
+
+    return $app;
+ ?>
